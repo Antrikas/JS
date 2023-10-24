74fe119409d399f902b0587778e8ddf101d7893d
Antrikas <andreas.antoniades17@gmail.com>
2023-10-24 13:37:47 +0300

    jscrashcourse

---
+++ app.js
@@ -0,0 +1,10 @@
+// celsius-to-fahrenheit formula:
+// F = C x 1.8 +32
+
+let celsious =10;
+let fahrenheit = celsious * 1.8 +32;
+
+console.log (fahrenheit)
+
+let bool = '1' === 1
+console.log(bool)
