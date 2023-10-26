2d8c6d73d7aa16a230d577c560d6375715eadc34
Antrikas <andreas.antoniades17@gmail.com>
2023-10-26 14:07:11 +0300

    jscrashcourse

--- app.js
+++ app.js
@@ -1,39 +1,4 @@
-let users = [{
-    username: "Andreas", 
-    email: "antrikos@gmail.com",
-    password:"1234", 
-    subscriptionStatus: "VIP",
-    discordId: "andreas#007", 
-    lessonsCompleted: [0, 1] 
-},
-{
-    username: "Matt", 
-    email: "matt@gmail.com",
-    password:"1434", 
-    subscriptionStatus: "VIP",
-    discordId: "matt#007", 
-    lessonsCompleted: [0, 1] 
-},
-{   username: "gio", 
-    email: "gio@gmail.com",
-    password:"15234", 
-    subscriptionStatus: "VIP",
-    discordId: "gior#007", 
-    lessonsCompleted: [0, 1] 
+function toggleDarkMode () {
+    document.querySelector("body").classList.toggle ("dark-theme")
 }
-]
 
-function register  (user) {
-    users.push (user);
-}
-   
-register ({
-username: "Andreas", 
-email: "antrikos@gmail.com",
- password: "1234",
- subscriptionStatus: "VIP",
- discordId: "andreas#006",
- lessonsCompleted: [0, 1] 
-});
-
- console.log (users);
