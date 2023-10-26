ea21881a867f712c48162e4d4f9343b1a447d6b7
Antrikas <andreas.antoniades17@gmail.com>
2023-10-26 09:53:46 +0300

    jscrashcourse

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
-// DRY = Don't repeat yourself
-//let count = 1;
+//DRY - Don't Reapeat Yourself
 
-//while (count <= 100) {
-  //  console.log (count)
-    //count = count + 1; 
-//}
-
-//for (let i = 0; i < 100; i++ ) {
-//console.log (i + 1);
-//}
-
-for (let i = 1; i <= 20; ++i) {
-console.log (i);
-
-if (i % 3 === 0 && i % 5 === 0) 
-console.log(`${i} -> Frontend Simplified`)
-
-else if (i % 3 === 0) 
-    console.log(`${i} -> Frontend`)
-
-else if (i % 5 === 0) 
-    console.log(`${i} -> Simplified`)
-
-else {
-    console.log (`${i} -> ${i}`)
+//Function Definition
+function welcomePersonToFES(firstName, lastName) {
+    console.log (`Welcome to FES, ${firstName} ${lastName} `)
 }
-   
-}
+
+//Call the Function
+welcomePersonToFES ( 'Andreas', 'Antoniades');
+welcomePersonToFES ('Giannis', 'Ioanou')
