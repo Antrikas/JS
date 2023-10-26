84a68469a4fffa1f4370dcb98e8cac093dc1ed9f
Antrikas <andreas.antoniades17@gmail.com>
2023-10-26 12:22:59 +0300

    jscrashcourse

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-let arr = [20, 30, 40, 50, 100]
+/* 
 
-let newArr = arr.filter ((element) => { 
-    console.log (element)
-    if (element < 50)
-    return true;
+Filter out all the 'FAIL' elements in an array
 
-})
+*/
 
-console.log (newArr)
+
+
+/*let arr = [ 1, 5, 10, 3]
+
+
+
+let newArray = arr.map((dollars) => dollars * 100)
+
+console.log (newArray);
+
+*/
+
+let dollars = [ 0, 10, 20]
+
+let cents = [];
+
+for (let i = 0; i < dollars.length; ++i) {
+    cents.push (dollars[i] * 100)
+    
+}
+    
+    console.log (cents)
+    
